5cd4aafa7fb6bcc17e40629e8a72a2446d4d1dde
erraghavchhabra <howtodonethis@gmial.com>
2025-06-25 17:34:03 +0530

    update again

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./pages/home";
 import Navbar from "./components/navbar";
 import TopBar from "./components/topBar";
 import "./App.css";
-import Footer from "./components/footer";
+import Footer from "./components/Footer";
 import About from "./pages/About";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import TermsAndConditions from "./pages/TermsAndConditions";
